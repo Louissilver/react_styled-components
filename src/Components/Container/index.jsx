42f4618dba8bdb5fe5162cd150ff6f30b21613ef
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-03-28 11:04:15 -0300

    Refatoração, herança e props

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-
+import styled from "styled-components";
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import { fundoClaro } from "../UI/variaveis";
 
-const Container = () => {
+const StyledContainer = styled.div`
+  background-color: ${fundoClaro};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+
+const StyledConteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export default () => {
   return (
-    <div className="container">
+    <StyledContainer>
       <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+      <StyledConteudo>
         <Conta />
-      </section>
-    </div>
+      </StyledConteudo>
+    </StyledContainer>
   );
 };
-
-export default Container;
